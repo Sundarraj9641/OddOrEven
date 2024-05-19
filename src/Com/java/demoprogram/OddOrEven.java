61b66b9b8dbaf62abf740efdac6abc858bbc4e00
Mahalingam <sundarraj.mahalingam@capgemini.com>
2024-05-19 10:25:37 +0530

    pushed by sundarraj

---
+++ src/Com/java/demoprogram/OddOrEven.java
@@ -0,0 +1,13 @@
+package Com.java.demoprogram;
+
+public class OddOrEven {
+	public static void main(String[] args) {
+		int a = 12;
+		if(a%2==0) {
+			System.out.println("even Number");
+		} else {
+			System.out.println("Odd Number");
+		}
+	}
+
+}
